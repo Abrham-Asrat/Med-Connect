c20fbfa171406ed45d102416471e984100df4fe6
Abrham-Asrat <abrhamasrat29@gmail.com>
2026-04-06 13:40:51 +0300

    updated:

--- BackendAPI/obj/Debug/net10.0/BackendAPI.AssemblyInfo.cs
+++ BackendAPI/obj/Debug/net10.0/BackendAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f336b52e4a813b7d9a80ff99af2c020a5963689c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20586bf8c46c1f3069094221fc6d556a5554b1b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
